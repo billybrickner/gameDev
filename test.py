96adc905cb38033f2474325af8819ca488bd665b
skullking419 <skullking419@gmail.com>
2018-12-30 20:46:12 -0500

    kkkkhhghfghfh

--- test.py
+++ test.py
@@ -1,29 +1,29 @@
 import random
 
 def hit(pip1, pip2, ac2):
-    values = [  ( 2, 19),
-                ( 3, 18),
-                ( 4, 17),
-                ( 6, 16),
-                ( 8, 15),
-                (1, 2)]
+    values = [  (  2, 19),
+                (  3, 18),
+                (  4, 17),
+                (  6, 16),
+                (  8, 15),
+                ( 11, 14)]
     r = random.randint(1,20)
     #print("Roll:", r)
     value = values[pip1]
     if r <= value[0]:
         r2 = random.randint(1,20)
-        if r2 > ac2 + 2*pip2 + 5:
-            return 15
+        if r2 > ac2 + 2*pip2 + 6:
+            return 10
         else:
             return 0
     elif r <= value[1]:
         r2 = random.randint(1,20)
         if r2 > ac2 + 2*pip2:
-            return 30
+            return 20
         else:
             return 0
     else:
-        return 45
+        return 40
 
 
 def main():
@@ -31,33 +31,40 @@ def main():
     for x in range(6):
         for y in range(6):
             p1 = 0
-            trials = 1000
+            trials = 100000
             for i in range(trials):
-                health1 = [700]
-                health2 = [700]
-                player = random.randint(1,2)
+                health1 = [70]
+                health2 = [60]
+                player = 1
                 while health1[0] > 0 and health2[0] > 0:
                     if player == 2:
                         player = 1
                         health = health2
-                        pip1 = x
-                        pip2 = y
+                        pip1 = y
+                        pip2 = x
 
                     else:
                         player = 2
                         health = health1
-                        pip1 = y
-                        pip2 = x
-                    health[0] -= hit(x,y,6)
+                        pip1 = x
+                        pip2 = y
+                    health[0] -= hit(pip1, 5 - pip2, 6)
                 # print("P1: ", health1[0])
                 # print("P2: ", health2[0])
                 if health1[0] > 1:
                     p1 += 1
             print("X: ", x, "Y: ", y)
-            print("Results: ", p1/trials)
+            # print("Results: ", p1/trials)
             winRate[x][y] = p1/trials
-    for row in winRate:
-        print(row)
+    print("     ", end="")
+    for i in range(6):
+        print(f"p1 {i+1:3d}", end=" ")
+    print()
+    for i, row in enumerate(winRate):
+        print(f"p2 {i+1:1d}", end=" ")
+        for elem in row:
+            print(f"{elem:0.4f}", end=" ")
+        print()
     
         
 
