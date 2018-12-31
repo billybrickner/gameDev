9826cba705e71e12d9f35e0b38d2f23e71cedb2b
Billy Brickner <billybrickner@gmail.com>
2018-12-30 16:14:02 -0800

    More trials

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ def hit(pip1, pip2, ac2):
                 ( 4, 17),
                 ( 6, 16),
                 ( 8, 15),
-                (11, 14)]
+                (1, 2)]
     r = random.randint(1,20)
     #print("Roll:", r)
     value = values[pip1]
@@ -27,25 +27,38 @@ def hit(pip1, pip2, ac2):
 
 
 def main():
-    p1 = 0
-    trials = 1000
-    for i in range(trials):
-        health1 = [700]
-        health2 = [700]
-        player = random.randint(1,2)
-        while health1[0] > 0 and health2[0] > 0:
-            if player == 2:
-                player = 1
-                health = health2
-            else:
-                player = 2
-                health = health1
-            health[0] -= hit(0,0,6)
-        # print("P1: ", health1[0])
-        # print("P2: ", health2[0])
-        if health1[0] > 1:
-            p1 += 1
-    print("Results: ", p1/trials)
+    winRate = [[0 for i in range(6)] for _ in range(6)]
+    for x in range(6):
+        for y in range(6):
+            p1 = 0
+            trials = 1000
+            for i in range(trials):
+                health1 = [700]
+                health2 = [700]
+                player = random.randint(1,2)
+                while health1[0] > 0 and health2[0] > 0:
+                    if player == 2:
+                        player = 1
+                        health = health2
+                        pip1 = x
+                        pip2 = y
+
+                    else:
+                        player = 2
+                        health = health1
+                        pip1 = y
+                        pip2 = x
+                    health[0] -= hit(x,y,6)
+                # print("P1: ", health1[0])
+                # print("P2: ", health2[0])
+                if health1[0] > 1:
+                    p1 += 1
+            print("X: ", x, "Y: ", y)
+            print("Results: ", p1/trials)
+            winRate[x][y] = p1/trials
+    for row in winRate:
+        print(row)
+    
         
 
 
